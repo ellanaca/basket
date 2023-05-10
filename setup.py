853267ea2a0dec846ecb04d3f455f1e34bb7f474
ellana <ellanaca1592@gmail.com>
2023-05-10 13:55:28 +0200

    espace

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
+
 setup(name='basket',
       version="1.0",
       description="Project NLP basket",
